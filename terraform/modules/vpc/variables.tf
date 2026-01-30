9f0ed2ba1faa38cf092c468c0b3c338966c4c0e9
YousifWafy <yousef.mohammad1@msa.edu.eg>
2026-01-30 11:27:42 +0200

    Add VPC module variables and outputs; restore nonprod.tfvars

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -12,8 +12,24 @@ variable "region" {
   description = "AWS region to deploy resources"
   type        = string
 }
+
 variable "availability_zone" {
   description = "AWS availability zone to deploy resources"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet"
   type        = string
 }
 
