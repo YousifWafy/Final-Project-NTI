8a3e9da1e7f1c19d85623d35e152e21dc686d2d4
YousifWafy <yousef.mohammad1@msa.edu.eg>
2026-01-30 16:04:19 +0200

    Refactor Terraform configuration files for improved readability and consistency

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "yousif-project-bucket"
-    key            = "terraform.tfstate"
-    region         = "ap-northeast-2"
-    encrypt        = true
+    bucket  = "yousif-project-bucket"
+    key     = "terraform.tfstate"
+    region  = "ap-northeast-2"
+    encrypt = true
   }
 }
