c89281117603225199a2615b7d0a5aa33906a90a
YousifWafy <yousef.mohammad1@msa.edu.eg>
2026-01-30 13:21:02 +0200

    Add VPC module configuration and update variable definitions in main.tf and variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
-variable "region" {
-  description = "AWS region"
+variable "environment" {
+  description = "Environment name"
   type        = string
 }
 
@@ -8,7 +8,29 @@ variable "project_name" {
   type        = string
 }
 
-variable "environment" {
-  description = "Environment name"
+variable "region" {
+  description = "AWS region to deploy resources"
   type        = string
 }
+
+variable "availability_zone" {
+  description = "AWS availability zone to deploy resources"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet"
+  type        = string
+}
+
+
