4553e48a8226858b08e915b3a6c22b22f61a1a9f
YousifWafy <yousef.mohammad1@msa.edu.eg>
2026-02-11 17:06:17 +0200

    Refactor infrastructure pipeline and Terraform backend configuration; streamline plan and apply stages, and update backend settings for clarity

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,12 @@
+# terraform {
+#   backend "s3" {
+#     bucket       = "yousif-project-bucket"
+#     region       = "ap-northeast-2"
+#     encrypt      = true
+#     use_lockfile = false
+#   }
+# }
+
 terraform {
-  backend "s3" {
-    bucket       = "yousif-project-bucket"
-    region       = "ap-northeast-2"
-    encrypt      = true
-    use_lockfile = false
-  }
+  backend "s3" {}
 }
